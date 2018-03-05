3b0b5e822164f1070367170e29aef8d62dec26d4
Alexander Menze <a.menze@polipol.de>
2018-03-05 09:19:24 +0100

    Added TreeNode class / Rewrite of OrderedJobsImpl

---
+++ CodeKata_OrderedJobs/0_Domain/TreeNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeKata_OrderedJobs.Domain
+{
+    class TreeNode<T>
+    {
+        public T Data { get; }
+        public TreeNode<T> Parent { get; }
+        public IEnumerable<TreeNode<T>> Children
+        {
+            get { return _children; }
+        }
+
+        private ICollection<TreeNode<T>> _children = new LinkedList<TreeNode<T>>();
+
+        public TreeNode(T data, TreeNode<T> parent = null){
+            Data = data;
+            Parent = parent;
+        }
+
+        public TreeNode<T> AddChild(T child)
+        {
+            var childNode = new TreeNode<T>(child, this);
+            _children.Add(childNode);
+            return childNode;
+        }
+
+        public TreeNode<T> Search(T data)
+            => Children.FirstOrDefault(node => node.Data.Equals(data));
+
+        public bool Contains(T data)
+            => Search(data) != null;
+
+        public string ToString()
+        {
+
+        }
+    }
+}
--- CodeKata_OrderedJobs/2_Infrastructure/OrderedJobsImpl.cs
+++ CodeKata_OrderedJobs/2_Infrastructure/OrderedJobsImpl.cs
@@ -1,4 +1,5 @@
 ﻿using CodeKata_OrderedJobs.BL;
+using CodeKata_OrderedJobs.Domain;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,27 +7,22 @@ namespace CodeKata_OrderedJobs.Infrastructure
 {
     public class OrderedJobsImpl : IOrderedJobs
     {
-        private IDictionary<char, char?> jobs = new Dictionary<char, char?>();
+        private TreeNode<char> jobsRoot = new TreeNode<char>(' ');
+        
 
         public void Register(char jobId, char dependsOn)
         {
-            if (jobs.ContainsKey(dependsOn))
-            {
-                jobs.Add(jobId, jobs[dependsOn] - 1);
-            }
+            jobsRoot.Search(jobId).AddChild(dependsOn);
         }
 
         public void Register(char jobId)
         {
-            if (jobs.ContainsKey(jobId))
-                return;
-
-            jobs.Add(jobId, int.MaxValue);
+            jobsRoot.AddChild(jobId);
         }
 
         public string Sort()
         {
-            return new string(jobs.OrderBy(job => job.Value).Select(job => job.Key).ToArray());
+            
         }
     }
 }
