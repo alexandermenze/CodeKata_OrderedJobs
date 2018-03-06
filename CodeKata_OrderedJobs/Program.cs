96ca0c14e4149f656a95eb83b770dc5ea1c6206e
Alexander Menze <a.menze@polipol.de>
2018-03-06 07:48:55 +0100

    Resharper Optimizations

--- CodeKata_OrderedJobs/Program.cs
+++ CodeKata_OrderedJobs/Program.cs
@@ -1,15 +1,8 @@
-﻿using CodeKata_OrderedJobs.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeKata_OrderedJobs
+﻿namespace CodeKata_OrderedJobs
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             
         }
