08fd95a1f64946ab2a8d677473597e806ba72db1
Alexander Menze <a.menze@polipol.de>
2018-03-06 12:57:04 +0100

    Removed empty lines from SplitToLines

--- CodeKata_OrderedJobs/Source/StringExtensions.cs
+++ CodeKata_OrderedJobs/Source/StringExtensions.cs
@@ -5,15 +5,19 @@ namespace CodeKata_OrderedJobs.Source
 {
     internal static class StringExtensions
     {
-        public static IEnumerable<string> SplitToLines(this string src)
+        public static IEnumerable<string> SplitToLinesIgnoringEmpty(this string src)
         {
             if (string.IsNullOrEmpty(src))
                 yield break;
 
             using (var stringReader = new StringReader(src))
             {
-                while (stringReader.Peek() != -1)
-                    yield return stringReader.ReadLine();
+                string readString;
+                while (stringReader.Peek() != -1 
+                       && !string.IsNullOrEmpty(readString = stringReader.ReadLine()))
+                {
+                    yield return readString;
+                }
             }
         }
     }
